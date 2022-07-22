a16ae044c7832868b10e45c813e08d8bb71a526a
Pheak02 <email@gmail.com>
2022-07-22 14:29:28 +0700

    added

---
+++ Prime 2.py
@@ -0,0 +1,7 @@
+for number in range:
+    if number >1: 
+        for i in range(1, 10):
+            if (number % i) == 0:
+                break
+            else: 
+                print(number) 
